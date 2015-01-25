9c8d7e136cd9c90fd53c2fae511a8d2b48392b4e
Yueh-Chun Tsai <birdtasi@gmail.com>
2015-01-25 13:27:21 +0800

    a

--- mysqlTest.php
+++ mysqlTest.php
@@ -1,3 +1,34 @@
 <?
-	echo "Hello World ";
+
+	$handle = fopen("../SQLinfo", "r");
+	if ($handle) {
+	    while (($line = fgets($handle)) !== false) {
+	         echo $line;
+	    }
+	    fclose($handle);
+	} else {
+	    // error opening the file.
+	} 
+
+
+	
+	// $server = "ServerID.database.windows.net,1433";
+	// $username = "Username@ServerID";
+	// $password = "Password";
+	// $database = "DbName";
+
+
+	// try
+	// {
+	//     $conn = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
+	// }
+	// catch(Exception $e)
+	// {
+	//     die(print_r($e));
+	// }
+
+
+
+
+
 ?>
